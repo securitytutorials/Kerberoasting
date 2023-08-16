eacba6ea36aeffce9e71d9536f056a3a9e6273bf
securitytutorials <56046244+securitytutorials@users.noreply.github.com>
2023-08-16 23:03:21 +0100

    Add files via upload

---
+++ CheckForKerberoasting.ps1
@@ -0,0 +1,12 @@
+﻿$KerbiEvents = Get-WinEvent -Logname security -FilterXPath "Event[System[(EventID=4769)]]and Event[EventData[Data[@Name='TicketEncryptionType']='0x17']]or Event[EventData[Data[@Name='TicketEncryptionType']='0x18']]" |
+
+Select-Object `
+@{Label='Time';Expression={$_.TimeCreated.ToString('g')}},
+@{Label='UserName';Expression={$_.Properties[0].Value}},
+@{Label='IPAddress';Expression={$_.Properties[6].Value}},
+@{Label="ServiceName";Expression={$_.properties[2].value}},
+@{Label="EncryptionType";Expression={$_.properties[5].value}}
+
+ 
+
+$KerbiEvents | Out-Gridview
